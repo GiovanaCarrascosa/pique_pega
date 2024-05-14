99bca3401743087bfa935835cf8b355e97649ad4
Giovana <giovanaa.carrascosa194@gmail.com>
2024-05-14 11:13:57 -0300

    add: cair de cima para baixo as coisas boas

--- jogo.py
+++ jogo.py
@@ -2,6 +2,7 @@ import pygame
 from personagem import Personagem
 from movimento import Obstaculo
 
+pontuacao = 0
 pygame.init()
 
 tela = pygame.display.set_mode ((800,600)) 
@@ -11,8 +12,13 @@ tela.fill((185,0,0))
 pygame.display.set_caption("A queda capilar") 
 
 lista_cabelos = [
-Obstaculo("imagens/cabelo cacheado.png", 140,90,276,465),
-Obstaculo("imagens/cabelo comprido.png", 110,130,456,252)
+Obstaculo("imagens/cabelo cacheado.png", 140,90,-10),
+Obstaculo("imagens/cabelo comprido.png", 110,130,-20),
+Obstaculo("imagens/cabelo harry styles.png", 100,90,-30),
+Obstaculo("imagens/cabelo preto.png", 100,90,-25),
+Obstaculo("imagens/cabelo loiro.png", 100,90,-15),
+Obstaculo("imagens/cabelo laranja.png", 120,90,-17),
+Obstaculo("imagens/topete menor.png", 100,90,-19),
 ]
 
 
@@ -41,7 +47,9 @@ while rodando:
         movimento.correr()
         movimento.desenhar(tela)
     
-      
+        if calvo.mascara.overlap(movimento.mascara,(movimento.pos_x - calvo.pos_x, movimento.pos_y - calvo.pos_y)):
+       
+            pontuacao = pontuacao + 1
  
     teclas = pygame.key.get_pressed()
 
@@ -53,12 +61,15 @@ while rodando:
         if calvo.pos_x > 0 :
             calvo.pos_x = calvo.pos_x - 7 #mudar de posição quando pressionar tecla para esquerda
 
-
+    
 
     
     calvo.desenhar(tela)
 
+    fonte = pygame.font.SysFont("Berlin Sans FB Demi", 18, False,False)
 
+    texto_pontuacao = fonte.render(f"Pontuação de pontos: {pontuacao}",True, (255,0,0)) 
+    tela.blit(texto_pontuacao,(3,5)) 
 
     pygame.display.update() 
 
--- movimento.py
+++ movimento.py
@@ -1,7 +1,7 @@
 import pygame
 import random
 class Obstaculo:
-    def __init__(self,arquivo_imagem,largura_imagem, altura_imagem,x_inicial,y_inicial): #função de construtor
+    def __init__(self,arquivo_imagem,largura_imagem, altura_imagem,y_inicial): #função de construtor
         self.imagem = pygame.image.load(arquivo_imagem)
 
         self.largura = largura_imagem
@@ -9,23 +9,21 @@ class Obstaculo:
 
         self.imagem = pygame.transform.scale(self.imagem,(self.largura,self.altura)) #tranformando o tamanho da imagem
 
-        self.pos_x = x_inicial
+        self.pos_x = random.randint(0, 780)
         self.pos_y = y_inicial
 
-        self.pox_x = x_inicial
-        self.pos_y - y_inicial
-
-        self.velocidade = random.randint(3, 10)
+    
+        self.velocidade = random.randint(2, 10)
 
         self.mascara = pygame.mask.from_surface (self.imagem)
 
     def correr(self):
-            if self.pos_x <=-100 :
-                 self.pos_x = 900
-            self.pos_x = self.pos_x - self.velocidade
+            if self.pos_y > 900 :
+                self.pos_y = -2000
+                self.pos_x = random.randint(0,780)
+            self.pos_y = self.pos_y + self.velocidade
 
-          
-
-
+           
+    
     def desenhar(self,tela):
         tela.blit(self.imagem,(self.pos_x,self.pos_y))
