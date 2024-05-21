6b4da94dc6f2d7e1de00a1e3b9d50ef453ecb831
giovana <giovanaa.carrascosa194@gmail.com>
2024-05-21 11:21:31 -0300

    add: vidas

--- jogo.py
+++ jogo.py
@@ -4,6 +4,7 @@ from personagem import Personagem
 from movimento import Obstaculo
 
 pontuacao = 0
+vida = 50
 pygame.init()
 
 tela = pygame.display.set_mode ((800,600)) 
@@ -13,16 +14,26 @@ tela.fill((185,0,0))
 pygame.display.set_caption("A queda capilar") 
 
 lista_cabelos = [
-Obstaculo("imagens/cabelo cacheado.png", 140,90,-10),
-Obstaculo("imagens/cabelo comprido.png", 110,130,-20),
-Obstaculo("imagens/cabelo harry styles.png", 100,90,-30),
-Obstaculo("imagens/cabelo preto.png", 100,90,-25),
-Obstaculo("imagens/cabelo loiro.png", 100,90,-15),
-Obstaculo("imagens/cabelo laranja.png", 120,90,-17),
-Obstaculo("imagens/topete menor.png", 100,90,-19),
+Obstaculo("imagens/cabelo cacheado.png", 100,60,-10),
+Obstaculo("imagens/cabelo comprido.png", 90,110,-20),
+Obstaculo("imagens/cabelo harry styles.png", 80,70,-30),
+Obstaculo("imagens/cabelo preto.png", 80,70,-25),
+Obstaculo("imagens/cabelo loiro.png", 80,70,-15),
+Obstaculo("imagens/cabelo laranja.png", 80,60,-17),
+Obstaculo("imagens/topete menor.png", 80,70,-19),
+Obstaculo("imagens/cabelo claro comprido.png", 90,110,-20),
+]
+
+lista_bomba = [
+Obstaculo("imagens/secador.png", 80,70,-19),
+Obstaculo("imagens/creme.png", 80,70,-19),
+Obstaculo("imagens/maquininha.png", 80,70,-19),
+Obstaculo("imagens/tesoura.png", 80,70,-19),
+Obstaculo("imagens/secador.png", 80,70,-19),
 ]
 
 
+    
 calvo = Personagem ("imagens/personagem calvo.png",170,180,295,420)
 
 
@@ -48,6 +59,7 @@ while rodando:
         movimento.correr()
         movimento.desenhar(tela)
     
+    
         if calvo.mascara.overlap(movimento.mascara,(movimento.pos_x - calvo.pos_x, movimento.pos_y - calvo.pos_y)):
             movimento.pos_y = -20
             movimento.pos_x = random.randint(1, 750)
@@ -64,7 +76,16 @@ while rodando:
             calvo.pos_x = calvo.pos_x - 7 #mudar de posição quando pressionar tecla para esquerda
 
     
+    for movimento in lista_bomba:
+        movimento.correr()
+        movimento.desenhar(tela)
 
+        if calvo.mascara.overlap(movimento.mascara,(movimento.pos_x - calvo.pos_x, movimento.pos_y - calvo.pos_y)):
+            movimento.pos_y = -20
+            movimento.pos_x = random.randint(1, 750)
+            
+            vida=vida-1
+ 
     
     calvo.desenhar(tela)
 
@@ -72,6 +93,11 @@ while rodando:
 
     texto_pontuacao = fonte.render(f"Pontuação de pontos: {pontuacao}",True, (255,0,0)) 
     tela.blit(texto_pontuacao,(3,5)) 
+
+    fonte = pygame.font.SysFont("Berlin Sans FB Demi", 18, False,False)
+
+    texto_pontuacao = fonte.render(f"Vida: {vida}",True, (255,0,0)) 
+    tela.blit(texto_pontuacao,(3,30)) 
 
     pygame.display.update() 
 
